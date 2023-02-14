73606d5f8e1480108459c467bab7de4ef5367233
dalenachau <missdalenachau@gmail.com>
2023-02-14 16:50:44 +0000

    fixed saving csv files to DB/data and refactored

--- src/conn.py
+++ src/conn.py
@@ -3,21 +3,22 @@ from dotenv import load_dotenv, find_dotenv
 from pg8000 import Connection
 import csv
 load_dotenv(find_dotenv())
-tote_user= os.environ.get("tote_user")
-tote_password= os.environ.get("tote_password")
+
 host='nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com'
 database='totesys'
-user=tote_user
-password=tote_password
-port=5432
-conn = Connection(user=user, password=password, host=host, database=database, port = port)
+user=os.environ.get("tote_user")
+password=os.environ.get("tote_password")
+
+conn = Connection(user=user, password=password, host=host, database=database)
 table_query = 'SELECT table_name FROM information_schema.tables WHERE table_schema = \'public\';'
 table_names = conn.run(table_query)
+if not os.path.exists('DB/data'):
+    os.makedirs('DB/data')
 for table_name in table_names:
     query = f"SELECT * FROM {table_name[0]}"
     table_data = conn.run(query)
-    with open(f"{table_name[0]}.csv", "w", newline="") as csvfile:
+    with open(f"DB/data/{table_name[0]}.csv", "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerows(table_data)
-print(table_names)
+
 conn.close()
