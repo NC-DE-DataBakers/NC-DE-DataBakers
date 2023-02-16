26e0f86c7545fed97f09ae21d2a1130f7dac889d
muaaz <muaaztest@gmail.com>
2023-02-16 09:53:57 +0000

    initial connection tests complete

--- tests/test_conn.py
+++ tests/test_conn.py
@@ -1,8 +1,11 @@
 from src.conn import lambda_handler
+from src.conn import conn_db
 from dotenv import load_dotenv, find_dotenv
 import os
-from pg8000 import Connection
+import pg8000
 import csv
+import boto3
+import pytest
 
 table_names = ['address', 'counterparty', 'currency', 'department', 'design', 'payment_type', 'payment', 'purchase_order', 'sales_order', 'staff', 'transaction']
 
@@ -14,19 +17,25 @@ def test_correct_credentials_are_retrieved():
     assert os.environ.get("tote_user") == "project_user_2"
     assert os.environ.get("tote_password") == "paxjekPK3hDXu2aXcJ9xyuBS"
 
+def test_connection_failss_when_credentials_are_correct():
+    load_dotenv(find_dotenv())
+    host=os.environ.get("tote_host")
+    database=os.environ.get("tote_database")
+    user=os.environ.get("tote_user")
+    password=os.environ.get("tote_password")
+    with pytest.raises(pg8000.exceptions.InterfaceError):
+        conn = pg8000.Connection("mybaduser", "password=password", "host=host", "database=database")
+def test_connection_user_for_interface_error():
+        try:
+            conn = pg8000.Connection("user=", "password=password", "host=host", "database=database")
+            return conn
+        except pg8000.exceptions.InterfaceError as IFE:
+            assert f"{IFE}" == "Can't create a connection to host password=password and port database=database (timeout is None and source_address is None)."
+
 def test_csv_file_is_created_for_each_table_name_containing_data_in_DB_data_directory():
     lambda_handler()
-    target_dir = "DB/data"
+    target_dir = "tmp"
     
     for table_name in table_names:
         csv_file = f"{target_dir}/{table_name}.csv"
         assert os.path.exists(csv_file)
-
-def test_valid_data_is_saved_to_CSV_file():
-    lambda_handler()
-    for table_name in table_names:
-        print(original_data)
-        with open(f'DB/data/{table_name}.csv', 'r') as csv_file:
-            csv_data = list(csv.reader(csv_file))
-            print(csv_data)
-        assert original_data == csv_data
