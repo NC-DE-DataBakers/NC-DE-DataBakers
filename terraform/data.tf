d78fa57881c28c6492ebeb7e50177080fcbc16bd
Klaudia Olkiewicz <dia.olkiewicz@gmail.com>
2023-02-17 12:19:20 +0000

    terraform plan 5

--- terraform/data.tf
+++ terraform/data.tf
@@ -4,14 +4,14 @@ data "aws_region" "current" {}
 
 data "archive_file" "tester_lambda" {
   type        = "zip"
-  source_file = "${path.module}/src/tester_lambda.py"
-  output_path = "${path.module}/terraform/code_zip/tester_lambda.zip"
+  source_file = "${path.module}/../src/tester_lambda.py"
+  output_path = "${path.module}/../tester_lambda.zip"
 }
 
 data "archive_file" "extractor_lambda" {
   type        = "zip"
-  source_file = "${path.module}/src/extractor_lambda.py"
-  output_path = "${path.module}/terraform/code_zip/extractor_lambda.zip"
+  source_file = "${path.module}/../src/extractor_lambda.py"
+  output_path = "${path.module}/../extractor_lambda.zip"
 }
 
 #if running terra locally use 
