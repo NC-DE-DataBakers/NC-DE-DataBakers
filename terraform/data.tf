50dca1173d8ac831071741a5aa038e6e73341404
Klaudia Olkiewicz <dia.olkiewicz@gmail.com>
2023-02-17 14:13:05 +0000

    terraform plan 11

--- terraform/data.tf
+++ terraform/data.tf
@@ -4,7 +4,7 @@ data "aws_region" "current" {}
 
 
 resource "random_id" "id" {
-  keepers {
+  keepers = {
     timestamp = timestamp() # force change on every execution
   }
 }
@@ -12,13 +12,13 @@ resource "random_id" "id" {
 data "archive_file" "tester_lambda" {
   type        = "zip"
   source_file = "${path.module}/../src/file_reader/tester_lambda.py"
-  output_path = "${path.module}/../tester_lambda.${resource.random_id.id.dec}zip"
+  output_path = "${path.module}/../tester_lambda.${resource.random_id.id.dec}-zip"
 }
 
 data "archive_file" "extractor_lambda" {
   type        = "zip"
   source_file = "${path.module}/../src/file_reader/extractor_lambda.py"
-  output_path = "${path.module}/../extractor_lambda.${resource.random_id.id.dec}zip"
+  output_path = "${path.module}/../extractor_lambda.${resource.random_id.id.dec}-zip"
 }
 
 #if running terra locally use 
