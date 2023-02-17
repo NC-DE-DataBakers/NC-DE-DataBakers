a8f23c15c70132c3255c733ce47257cf8ab131f0
Klaudia Olkiewicz <dia.olkiewicz@gmail.com>
2023-02-17 14:42:07 +0000

    fix

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,25 +3,25 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 
-resource "random_id" "id" {
-  byte_length = 8
+# resource "random_id" "id" {
+#   byte_length = 8
 
-  keepers = {
-    timestamp = timestamp() # force change on every execution
-  }
-}
+#   keepers = {
+#     timestamp = timestamp() # force change on every execution
+#   }
+# }
 
-data "archive_file" "tester_lambda" {
-  type        = "zip"
-  source_file = "${path.module}/../src/file_reader/tester_lambda.py"
-  output_path = "${path.module}/../tester_lambda.${resource.random_id.id.dec}-zip"
-}
+# data "archive_file" "tester_lambda" {
+#   type        = "zip"
+#   source_file = "${path.module}/../src/tester_lambda.py"
+#   output_path = "${path.module}/../tester_lambda.${resource.random_id.id.dec}-zip"
+# }
 
-data "archive_file" "extractor_lambda" {
-  type        = "zip"
-  source_file = "${path.module}/../src/file_reader/extractor_lambda.py"
-  output_path = "${path.module}/../extractor_lambda.${resource.random_id.id.dec}-zip"
-}
+# data "archive_file" "extractor_lambda" {
+#   type        = "zip"
+#   source_file = "${path.module}/../src/extractor_lambda.py"
+#   output_path = "${path.module}/../extractor_lambda.${resource.random_id.id.dec}-zip"
+# }
 
 #if running terra locally use 
 #output_path = "${path.module}/../function.zip"
