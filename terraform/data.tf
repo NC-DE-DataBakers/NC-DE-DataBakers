e349abbbefc6042b3cbeac35246f4f9588f98117
Klaudia Olkiewicz <dia.olkiewicz@gmail.com>
2023-02-17 14:04:20 +0000

    terraform plan 9

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,16 +3,22 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 
+resource "random_id" "id" {
+  keepers {
+    timestamp = "${timestamp()}" # force change on every execution
+  }
+}
+
 data "archive_file" "tester_lambda" {
   type        = "zip"
   source_file = "${path.module}/../src/file_reader/tester_lambda.py"
-  output_path = "${path.module}/../tester_lambda.zip"
+  output_path = "${path.module}/../tester_lambda.${resource.random_id.id.dec}zip"
 }
 
 data "archive_file" "extractor_lambda" {
   type        = "zip"
   source_file = "${path.module}/../src/file_reader/extractor_lambda.py"
-  output_path = "${path.module}/../extractor_lambda.zip"
+  output_path = "${path.module}/../extractor_lambda.${resource.random_id.id.dec}zip"
 }
 
 #if running terra locally use 
