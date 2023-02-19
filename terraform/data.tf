1b24d8c1eca376d2f3a45057326014bbe2f1db1b
Klaudia Olkiewicz <dia.olkiewicz@gmail.com>
2023-02-19 19:24:57 +0000

    fix zip 3

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,14 +2,14 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-data "file" "tester_lambda" {
+data "archive_file" "tester_lambda" {
   type        = "zip"
   source_file = "${path.module}/../src/tester_lambda.py"
   output_path = "${path.module}/../terraform/code_zip/tester_lambda.zip"
 }
 
 
-data "file" "extractor_lambda" {
+data "archive_file" "extractor_lambda" {
   type        = "zip"
   source_file = "${path.module}/../src/tester_lambda.py"
   output_path = "${path.module}/../terraform/code_zip/extractor_lambda.zip"
