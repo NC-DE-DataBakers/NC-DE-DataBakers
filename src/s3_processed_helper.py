73e545e8d99dd8d15cb15b98042b43dd0ddf1423
muaaz <muaaztest@gmail.com>
2023-02-21 09:39:31 +0000

    Completed error handlingfor processed s3 helper (upload pqt and delete csv)

--- src/s3_processed_helper.py
+++ src/s3_processed_helper.py
@@ -10,11 +10,11 @@ def s3_list_buckets():
 def s3_list_prefix_buckets():
     full_list = s3_list_buckets()
     if full_list == []:
-        raise ValueError("No buckets found")
+        raise ValueError("ERROR: No buckets found")
     for bucket in full_list:
         if "nc-de-databakers-csv-store-" in bucket:
             return bucket
-    raise ValueError("Prefix not found in any bucket")
+    raise ValueError("ERROR: Prefix not found in any bucket")
 
 def s3_setup_success():
     s3=boto3.client('s3')
@@ -23,6 +23,8 @@ def s3_setup_success():
     for object in objects:
         if object['Key'] == 'processed_csv_key/setup_success_csv_processed.txt':
             return True
+    else:
+        raise ValueError("ERROR: Terraform deployment unsuccessful")
 
 def s3_upload_pqt_files():
     s3=boto3.resource('s3')
@@ -31,7 +33,7 @@ def s3_upload_pqt_files():
         for file in pqt_files:
             s3.Bucket(s3_list_prefix_buckets()).upload_file(f'./pqt_tmp/{file}', f'processed_csv_key/{file}')
     else:
-        raise ValueError("Terraform deployment unsuccessful")
+        raise ValueError("ERROR: Terraform deployment unsuccessful")
 
 def s3_upload_pqt_and_delete_csv_files_from_input_key():
     s3_upload_and_local_log
