78753d3974d6a382030a7432993b6cb555947a61
mattlaw9000 <matlaw9000@gmail.com>
2023-02-21 12:31:15 +0000

    csv to parquet conversion completed, tested - comments still to be left, requirements updated

--- src/csv_to_parquet.py
+++ src/csv_to_parquet.py
@@ -3,24 +3,25 @@ import pandas as pd
 import glob
 import os
 
-s3=boto3.client('s3')
 def s3_list_buckets():
-    response=s3.list_buckets()['Buckets']
-    try:
-      return [bucket['Name'] for bucket in response]
-    except:
-      raise ValueError("ERROR: No buckets found")
+  s3=boto3.client('s3')
+  response=s3.list_buckets()['Buckets']
+  try:
+    return [bucket['Name'] for bucket in response]
+  except:
+    raise ValueError("ERROR: No buckets found")
 
 def s3_parquet_prefix_buckets():
     full_list = s3_list_buckets()
     if full_list == []:
         raise ValueError("ERROR: No buckets found")
     for bucket in full_list:
-        if "nc-de-databakers-parquet-store" in bucket:
+        if "nc-de-databakers-csv-store" in bucket:
             return bucket
     raise ValueError("ERROR: Prefix not found in any bucket")
 
 def list_files_to_convert():
+  s3=boto3.client('s3')
   bucket = s3_parquet_prefix_buckets()
   list=s3.list_objects(Bucket=bucket)['Contents']
   for key in list:
@@ -28,8 +29,34 @@ def list_files_to_convert():
 
 def convert_csv_to_parquet():
   file_list = glob.glob("./tmp/*.csv")
+  if len(file_list) < 1:
+    raise ValueError('ERROR: No CSV files to convert to parquet')
   for file in file_list:
     filename = os.path.basename(file).split('.')[0]
     df = pd.read_csv(file)
-    df.to_parquet(f'./pqt_tmp/{filename}.parquet')
+    
+    if not os.path.exists("pqt_tmp"):
+        os.makedirs("pqt_tmp")
+    try:
+      df.to_parquet(f'./pqt_tmp/{filename}.parquet')
+    except pd.errors.EmptyDataError as EDE:
+      raise f'ERROR: {EDE}'
+    except Exception:
+      raise f'ERROR: {Exception}'
+    
+def update_csv_conversion_file():
+  s3=boto3.client('s3')
+  full_list = s3_list_buckets()
+  bucket_str = ''
+  for bucket in full_list:
+      if "nc-de-databakers-csv-store" in bucket:
+            bucket_str = bucket
+  s3.download_file(bucket_str, 'processed_csv_key/csv_conversion.txt', './tmp/csv_conversion.txt')
+  
+  contents = open('./tmp/csv_conversion.txt', 'r').read()
+  num = int(contents.split(' ')[1])
+  with open('./tmp/csv_conversion.txt', 'w+') as file:
+    file.write(f'Run {num+1}')
+  s3.upload_file("./tmp/csv_conversion.txt", bucket_str, "processed_csv_key/csv_conversion.txt")
+  
   
--- tests/test_csv_to_parquet.py
+++ tests/test_csv_to_parquet.py
@@ -3,8 +3,10 @@ import boto3
 import pytest
 from unittest.mock import patch
 import os
-from src.csv_to_parquet import convert_csv_to_parquet, list_files_to_convert
+import csv
+from src.csv_to_parquet import convert_csv_to_parquet, list_files_to_convert, update_csv_conversion_file
 import src.csv_to_parquet
+import pandas as pd
 
 @mock_s3
 def test_connection_to_bucket():
@@ -12,7 +14,6 @@ def test_connection_to_bucket():
   response=s3.list_buckets()['Buckets']
   with pytest.raises(ValueError):
     try:
-      print(response)
       assert len([bucket['Name'] for bucket in response]) > 0
     except:
       raise ValueError("ERROR: No buckets found")
@@ -22,10 +23,85 @@ def test_list_files_to_convert():
   s3 = boto3.client("s3")
   s3.create_bucket(Bucket='parquet-test-bucket')
   with patch ('src.csv_to_parquet.s3_parquet_prefix_buckets', return_value="parquet-test-bucket"):
-    s3.put_object(Body="./setup_success_csv_input.txt", Bucket="parquet-test-bucket", Key="setup_success_csv_input.txt")
+    open('mocks3testfile.txt', 'w')
+    s3.put_object(Body="./mocks3testfile.txt", Bucket="parquet-test-bucket", Key="mocks3testfile.txt")
     
     list=s3.list_objects(Bucket='parquet-test-bucket')['Contents']
     for key in list:
       s3.download_file('parquet-test-bucket', key['Key'], f'./tmp/{key["Key"]}')
     
-    assert os.path.isfile("./tmp/setup_success_csv_input.txt")
+    assert os.path.isfile("./tmp/mocks3testfile.txt")
+    os.remove('./tmp/mocks3testfile.txt')
+    assert os.path.isfile("./tmp/mocks3testfile.txt") is False
+    
+@mock_s3
+def test_files_are_converted_to_parquet():
+  fields = ['column1', 'column2', 'column3']  
+  rows = [ ['A', '011', '2000'], 
+        ['B', '012', '8000'],
+        ['C', '351', '5000'],
+        ['D', '146', '10000'] ] 
+  
+  with open('./tmp/mockconversion1.csv', 'w') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(fields)
+    csv_writer.writerows(rows)
+    
+  reader = csv.reader(open("./tmp/mockconversion1.csv"))
+  # for row in reader:
+  #     print(row)
+  convert_csv_to_parquet()
+  
+  assert os.path.isfile("./pqt_tmp/mockconversion1.parquet")
+  os.remove('./pqt_tmp/mockconversion1.parquet')
+  os.removedirs('./pqt_tmp')
+  
+  assert os.path.isfile("./pqt_tmp/mockconversion1.parquet") is False
+  assert os.path.isdir('./pqt_tmp') is False
+  
+def test_files_are_converted_to_parquet():
+  open('./tmp/mockconversion1.csv', 'w')
+  with pytest.raises(pd.errors.EmptyDataError):
+    convert_csv_to_parquet()
+  os.remove('./tmp/mockconversion1.csv')
+  
+  assert os.path.isfile("./tmp/mockconversion1.csv") is False
+
+def test_ValueError_raised_for_empty_tmp_folder():
+  with pytest.raises(ValueError):
+    convert_csv_to_parquet()
+
+@mock_s3   
+def test_csv_conversion_file_downloaded():
+  s3 = boto3.client("s3")
+  bucket = 'nc-de-databakers-csv-store-1010'
+  s3.create_bucket(Bucket=bucket)
+  with open('./csv_conversion.txt', 'w') as file:
+    file.write('Run 0')
+  s3.upload_file("./csv_conversion.txt", bucket, "processed_csv_key/csv_conversion.txt")
+  with patch ('src.csv_to_parquet.s3_list_buckets', return_value=[bucket]):
+    update_csv_conversion_file()
+    
+  assert os.path.isfile("./tmp/csv_conversion.txt")
+  os.remove('./tmp/csv_conversion.txt')
+  assert os.path.isfile("./tmp/csv_conversion.txt") is False
+
+@mock_s3
+def test_csv_conversion_run_number_incremented():
+  s3 = boto3.client("s3")
+  bucket = 'nc-de-databakers-csv-store-1010'
+  s3.create_bucket(Bucket=bucket)
+  with open('./csv_conversion.txt', 'wb') as file:
+    file.write(b'Run 0')
+  s3.upload_file("./csv_conversion.txt", bucket, "processed_csv_key/csv_conversion.txt")
+  with patch ('src.csv_to_parquet.s3_list_buckets', return_value=[bucket]):
+    update_csv_conversion_file()
+    
+  with open('./tmp/csv_conversion.txt', 'r') as file:
+    contents = file.read()
+    assert contents == 'Run 1'
+  
+  s3.download_file(bucket, 'processed_csv_key/csv_conversion.txt', './tmp/csv_conversion_s3.txt')
+  with open('./tmp/csv_conversion_s3.txt', 'r') as file:
+    contents = file.read()
+    assert contents == 'Run 1'
