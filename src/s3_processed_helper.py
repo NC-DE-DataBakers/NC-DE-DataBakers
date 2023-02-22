6d79ff6e27890eb9fdea3dda5868f6aad7d83872
muaaz <muaaztest@gmail.com>
2023-02-22 10:39:22 +0000

    create pqt input bucket upload and csv processed pqt file delete functionality with err handling and tdd (run log remaining)

--- src/s3_processed_helper.py
+++ src/s3_processed_helper.py
@@ -7,7 +7,7 @@ def s3_list_buckets():
     response=s3.list_buckets()['Buckets']
     return [bucket['Name'] for bucket in response]
 
-def s3_list_prefix_buckets():
+def s3_list_prefix_csv_buckets():
     full_list = s3_list_buckets()
     if full_list == []:
         raise ValueError("ERROR: No buckets found")
@@ -16,34 +16,71 @@ def s3_list_prefix_buckets():
             return bucket
     raise ValueError("ERROR: Prefix not found in any bucket")
 
-def s3_setup_success():
+def s3_list_prefix_parquet_buckets():
+    full_list = s3_list_buckets()
+    if full_list == []:
+        raise ValueError("ERROR: No buckets found")
+    for bucket in full_list:
+        if "nc-de-databakers-parquet-store-" in bucket:
+            return bucket
+    raise ValueError("ERROR: Prefix not found in any bucket")
+
+def s3_csv_processed_setup_success():
     s3=boto3.client('s3')
-    processed_bucket = s3_list_prefix_buckets()
-    objects = s3.list_objects(Bucket=processed_bucket)['Contents']
+    bucket = s3_list_prefix_csv_buckets()
+    objects = s3.list_objects(Bucket=bucket)['Contents']
     for object in objects:
         if object['Key'] == 'processed_csv_key/setup_success_csv_processed.txt':
             return True
     else:
         raise ValueError("ERROR: Terraform deployment unsuccessful")
 
-def s3_upload_pqt_files():
+
+def s3_upload_pqt_files_to_csv_processed_key():
     s3=boto3.resource('s3')
-    if s3_setup_success():
+    if s3_csv_processed_setup_success():
         pqt_files = os.listdir('pqt_tmp')
         for file in pqt_files:
-            s3.Bucket(s3_list_prefix_buckets()).upload_file(f'./pqt_tmp/{file}', f'processed_csv_key/{file}')
+            s3.Bucket(s3_list_prefix_csv_buckets()).upload_file(f'./pqt_tmp/{file}', f'processed_csv_key/{file}')
+    else:
+        raise ValueError("ERROR: Terraform deployment unsuccessful")
+
+def s3_upload_pqt_files_to_pqt_input_key():
+    s3=boto3.resource('s3')
+    if s3_pqt_input_setup_success():
+        pqt_files = os.listdir('pqt_tmp')
+        for file in pqt_files:
+            s3.Bucket(s3_list_prefix_parquet_buckets()).upload_file(f'./pqt_tmp/{file}', f'input_parquet_key/{file}')
     else:
         raise ValueError("ERROR: Terraform deployment unsuccessful")
 
 def s3_upload_pqt_and_delete_csv_files_from_input_key():
-    s3_upload_and_local_log
-    s3_upload_pqt_files
+    s3_upload_pqt_files_to_csv_processed_key()
     s3=boto3.resource('s3')
-    bucket_name = s3_list_prefix_buckets()
+    bucket_name = s3_list_prefix_csv_buckets()
     bucket = s3.Bucket(bucket_name)
     for obj in bucket.objects.filter(Prefix='input_csv_key/'):
         if obj.key.endswith('.csv'):
             obj.delete()
+
+def s3_upload_pqt_and_delete_pqt_files_from_processed_key():
+    s3_upload_pqt_files_to_pqt_input_key()
+    s3=boto3.resource('s3')
+    bucket_name = s3_list_prefix_csv_buckets()
+    bucket = s3.Bucket(bucket_name)
+    for obj in bucket.objects.filter(Prefix='processed_csv_key/'):
+        if obj.key.endswith('.parquet'):
+            obj.delete()
+
+def s3_pqt_input_setup_success():
+    s3=boto3.client('s3')
+    bucket = s3_list_prefix_parquet_buckets()
+    objects = s3.list_objects(Bucket=bucket)['Contents']
+    for object in objects:
+        if object['Key'] == 'input_parquet_key/setup_success_parquet_input.txt':
+            return True
+    else:
+        raise ValueError("ERROR: Terraform deployment unsuccessful")
 
 def create_pqt_completed_txt_file():
     s3_upload_pqt_and_delete_csv_files_from_input_key()
@@ -60,6 +97,9 @@ def create_pqt_completed_txt_file():
 def s3_pqt_upload_and_log():
     create_pqt_completed_txt_file()
     s3=boto3.resource('s3')
-    s3.Bucket(s3_list_prefix_buckets()).upload_file('./pqt_export_completed.txt', 'processed_csv_key/pqt_export_completed.txt')
+    s3.Bucket(s3_list_prefix_csv_buckets()).upload_file('./pqt_export_completed.txt', 'processed_csv_key/pqt_export_completed.txt')
 
-s3_pqt_upload_and_log()
+# def s3_move_pqt_to_parquet_bucket_input_key_and_delete_parquet_from_csv_bucket_processed_key():
+#     s3_upload_pqt_and_delete_csv_files_from_input_key()
+#     s3=boto3.resource('s3')
+#     bucket_name=s3_list_prefix_csv_buckets()
--- tests/test_s3_processed_helper.py
+++ tests/test_s3_processed_helper.py
@@ -1,4 +1,4 @@
-from src.s3_processed_helper import s3_list_buckets, s3_list_prefix_buckets, s3_setup_success, s3_upload_pqt_files, s3_upload_pqt_and_delete_csv_files_from_input_key, create_pqt_completed_txt_file, s3_pqt_upload_and_log
+from src.s3_processed_helper import s3_list_buckets, s3_list_prefix_csv_buckets,s3_list_prefix_parquet_buckets, s3_csv_processed_setup_success, s3_upload_pqt_files_to_csv_processed_key, s3_upload_pqt_and_delete_csv_files_from_input_key, create_pqt_completed_txt_file, s3_pqt_upload_and_log, s3_pqt_input_setup_success,s3_upload_pqt_files_to_pqt_input_key, s3_upload_pqt_and_delete_pqt_files_from_processed_key
 import boto3
 from moto import mock_s3
 import os
@@ -21,15 +21,23 @@ def test_multiple_s3_list_buckets():
     assert 'my-test-bucket-2' in bucket_names
 
 @mock_s3
-def test_bucket_list_for_prefix():
+def test_bucket_list_for_csv_prefix():
     s3 = boto3.client('s3')
     s3.create_bucket(Bucket='nc-de-databakers-csv-store-20202')
     s3.create_bucket(Bucket='my-test-bucket')
-    bucket_names = s3_list_prefix_buckets()
+    bucket_names = s3_list_prefix_csv_buckets()
     assert bucket_names == 'nc-de-databakers-csv-store-20202'
 
 @mock_s3
-def test_bucket_for_input_folder_if_bucket_is_prefixed():
+def test_bucket_list_for_parquet_prefix():
+    s3 = boto3.client('s3')
+    s3.create_bucket(Bucket='nc-de-databakers-parquet-store-20202')
+    s3.create_bucket(Bucket='my-test-bucket')
+    bucket_names = s3_list_prefix_parquet_buckets()
+    assert bucket_names == 'nc-de-databakers-parquet-store-20202'
+
+@mock_s3
+def test_csv_bucket_for_processed_key_if_bucket_is_prefixed():
     s3 = boto3.client('s3')
     s3.create_bucket(Bucket='nc-de-databakers-csv-store-20202')
     s3.put_object(Bucket='nc-de-databakers-csv-store-20202',
@@ -38,14 +46,23 @@ def test_bucket_for_input_folder_if_bucket_is_prefixed():
     assert "processed_csv_key" in object_names["Contents"][0]["Key"]
 
 @mock_s3
-def test_setup_success_txt_file_exists():
+def test_parquet_bucket_for_input_key_if_bucket_is_prefixed():
+    s3 = boto3.client('s3')
+    s3.create_bucket(Bucket='nc-de-databakers-parquet-store-20202')
+    s3.put_object(Bucket='nc-de-databakers-parquet-store-20202',
+                  Key='input_parquet_key/')
+    object_names = s3.list_objects(Bucket='nc-de-databakers-parquet-store-20202')
+    assert "input_parquet_key" in object_names["Contents"][0]["Key"]
+
+@mock_s3
+def test_setup_success_txt_file_exists_csv_processed():
     s3 = boto3.client('s3')
     s3.create_bucket(Bucket='nc-de-databakers-csv-store-20202')
     s3.put_object(Bucket='nc-de-databakers-csv-store-20202', Key='processed_csv_key/')
     s3res = boto3.resource('s3')
     s3res.Bucket('nc-de-databakers-csv-store-20202').upload_file(
         './setup_success_csv_processed.txt', 'processed_csv_key/setup_success_csv_processed.txt')
-    assert s3_setup_success() == True
+    assert s3_csv_processed_setup_success() == True
 
 @mock_s3
 def test_parquet_files_are_uploaded_successfully_to_the_processed_key_within_bucket():
@@ -56,13 +73,14 @@ def test_parquet_files_are_uploaded_successfully_to_the_processed_key_within_buc
     s3res = boto3.resource('s3')
     s3res.Bucket('nc-de-databakers-csv-store-20202').upload_file(
         './setup_success_csv_processed.txt', 'processed_csv_key/setup_success_csv_processed.txt')
-    s3_upload_pqt_files()
+    s3_upload_pqt_files_to_csv_processed_key()
     objects = s3cli.list_objects(
         Bucket='nc-de-databakers-csv-store-20202', Prefix='processed_csv_key/')['Contents']
     keys = [object['Key'] for object in objects]
     assert all(f'processed_csv_key/{file}' in keys for file in pqt_files)
 
-def test_upload_pqt_and_delete_input_csv_files():
+@mock_s3
+def test_upload_pqt_to_processed_csv_and_delete_input_csv_files():
     s3cli = boto3.client('s3')
     s3cli.create_bucket(Bucket='nc-de-databakers-csv-store-20202')
     s3cli.put_object(Bucket='nc-de-databakers-csv-store-20202',
@@ -79,6 +97,56 @@ def test_upload_pqt_and_delete_input_csv_files():
     csv_count = sum([1 for obj in input_csv_objects if obj.endswith('.csv')])
     assert csv_count == 0
 
+@mock_s3
+def test_setup_success_txt_file_exists_pqt_input():
+    s3 = boto3.client('s3')
+    s3.create_bucket(Bucket='nc-de-databakers-parquet-store-20202')
+    s3.put_object(Bucket='nc-de-databakers-parquet-store-20202', Key='input_csv_key/')
+    s3res = boto3.resource('s3')
+    s3res.Bucket('nc-de-databakers-parquet-store-20202').upload_file(
+        './setup_success_parquet_input.txt', 'input_parquet_key/setup_success_parquet_input.txt')
+    assert s3_pqt_input_setup_success() == True
+
+@mock_s3
+def test_parquet_files_are_uploaded_successfully_to_the_input_key_within_pqt_bucket():
+    s3cli = boto3.client('s3')
+    pqt_files = ['address.parquet', 'counterparty.parquet', 'currency.parquet', 'department.parquet', 'design.parquet','payment_type.parquet', 'payment.parquet', 'purchase_order.parquet', 'sales_order.parquet', 'staff.parquet', 'transaction.parquet']
+    s3cli.create_bucket(Bucket='nc-de-databakers-parquet-store-20202')
+    s3cli.put_object(Bucket='nc-de-databakers-parquet-store-20202',Key='input_parquet_key/')
+    s3res = boto3.resource('s3')
+    s3res.Bucket('nc-de-databakers-parquet-store-20202').upload_file(
+        './setup_success_parquet_input.txt', 'input_parquet_key/setup_success_parquet_input.txt')
+    s3_upload_pqt_files_to_pqt_input_key()
+    objects = s3cli.list_objects(
+        Bucket='nc-de-databakers-parquet-store-20202', Prefix='input_parquet_key/')['Contents']
+    keys = [object['Key'] for object in objects]
+    assert all(f'input_parquet_key/{file}' in keys for file in pqt_files)
+
+@mock_s3
+def test_upload_pqt_to_processed_csv_and_delete_input_csv_files():
+    s3cli = boto3.client('s3')
+    s3cli.create_bucket(Bucket='nc-de-databakers-csv-store-20202')
+    s3cli.put_object(Bucket='nc-de-databakers-csv-store-20202',
+                     Key='input_csv_key/')
+    s3cli.put_object(Bucket='nc-de-databakers-csv-store-20202',
+                     Key='processed_csv_key/')
+    s3cli.create_bucket(Bucket='nc-de-databakers-parquet-store-20202')
+    s3cli.put_object(Bucket='nc-de-databakers-parquet-store-20202',
+                     Key='input_csv_key/')
+    s3res = boto3.resource('s3')
+    s3res.Bucket('nc-de-databakers-csv-store-20202').upload_file(
+        './setup_success_csv_input.txt', 'input_csv_key/setup_success_csv_input.txt')
+    s3res.Bucket('nc-de-databakers-csv-store-20202').upload_file(
+        './setup_success_csv_processed.txt', 'processed_csv_key/setup_success_csv_processed.txt')
+    s3res.Bucket('nc-de-databakers-parquet-store-20202').upload_file(
+        './setup_success_parquet_input.txt', 'input_parquet_key/setup_success_parquet_input.txt')
+    s3_upload_pqt_and_delete_csv_files_from_input_key()
+    s3_upload_pqt_and_delete_pqt_files_from_processed_key()
+    processed_csv_objects = [obj.key for obj in s3res.Bucket('nc-de-databakers-csv-store-20202').objects.filter(Prefix='processed_csv_key/')]
+    pqt_count = sum([1 for obj in processed_csv_objects if obj.endswith('.parquet')])
+    assert pqt_count == 0
+
+@mock_s3
 def test_creates_pqt_export_completed_txt_file_if_files_are_uploaded():
     s3cli = boto3.client('s3')
     s3cli.create_bucket(Bucket='nc-de-databakers-csv-store-20202')
@@ -132,26 +200,49 @@ def test_uploads_pqt_export_file_to_key_within_bucket():
     assert 'processed_csv_key/pqt_export_completed.txt' in keys
 
 @mock_s3
-def test_list_prefix_buckets_raises_exception_when_there_are_no_buckets():
+def test_list_csv_prefix_buckets_raises_exception_when_there_are_no_buckets():
     with pytest.raises(ValueError) as errinfo:
-        s3_list_prefix_buckets()
+        s3_list_prefix_csv_buckets()
     assert str(errinfo.value) == "ERROR: No buckets found"
 
 @mock_s3
-def test_list_prefix_buckets_raises_exception_when_there_are_no_prefix_buckets():
+def test_list_csv_prefix_buckets_raises_exception_when_there_are_no_prefix_buckets():
     s3 = boto3.client('s3')
     s3.create_bucket(Bucket='test_bucket_val_error_707352')
     with pytest.raises(ValueError) as errinfo:
-        s3_list_prefix_buckets()
+        s3_list_prefix_csv_buckets()
     assert str(errinfo.value) == "ERROR: Prefix not found in any bucket"
 
 @mock_s3
-def test_setup_unsuccessful_error_message():
+def test_setup_unsuccessful_error_message_for_csv_processed():
     s3 = boto3.client('s3')
     s3.create_bucket(Bucket='nc-de-databakers-csv-store-20202')
     s3.put_object(Bucket='nc-de-databakers-csv-store-20202',Key='processed_csv_key/')
     with pytest.raises(ValueError) as errinfo:
-        s3_upload_pqt_files()
+        s3_upload_pqt_files_to_csv_processed_key()
+    assert str(errinfo.value) == "ERROR: Terraform deployment unsuccessful"
+
+@mock_s3
+def test_list_pqt_prefix_buckets_raises_exception_when_there_are_no_buckets():
+    with pytest.raises(ValueError) as errinfo:
+        s3_list_prefix_parquet_buckets()
+    assert str(errinfo.value) == "ERROR: No buckets found"
+
+@mock_s3
+def test_list_parquet_prefix_buckets_raises_exception_when_there_are_no_prefix_buckets():
+    s3 = boto3.client('s3')
+    s3.create_bucket(Bucket='test_bucket_val_error_707352')
+    with pytest.raises(ValueError) as errinfo:
+        s3_list_prefix_parquet_buckets()
+    assert str(errinfo.value) == "ERROR: Prefix not found in any bucket"
+
+@mock_s3
+def test_setup_unsuccessful_error_message_for_parquet_input():
+    s3 = boto3.client('s3')
+    s3.create_bucket(Bucket='nc-de-databakers-parquet-store-20202')
+    s3.put_object(Bucket='nc-de-databakers-parquet-store-20202',Key='input_csv_key/')
+    with pytest.raises(ValueError) as errinfo:
+        s3_upload_pqt_files_to_pqt_input_key()
     assert str(errinfo.value) == "ERROR: Terraform deployment unsuccessful"
 
 if os.path.exists("pqt_run_number.txt"):
