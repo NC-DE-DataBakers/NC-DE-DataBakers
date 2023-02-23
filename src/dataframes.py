2e812228a12233db9ffeef4e04ac8f1541ea5d6b
dalenachau <missdalenachau@gmail.com>
2023-02-23 16:29:41 +0000

    adds helper function for dim_staff

--- src/dataframes.py
+++ src/dataframes.py
@@ -14,8 +14,11 @@ import os
 #     return file_data
 
 def dim_staff():
-    # staff_id, first_name, last_name, department_name, location, email_address
-    pass
+    staff_df = pd.read_csv('./tmp/staff.csv')
+    dept_df = pd.read_csv('./tmp/department.csv')
+    staff_dept_df = pd.merge(staff_df, dept_df, on='department_id').sort_values('staff_id')
+    dim_staff = pd.DataFrame(data={'staff_id': staff_dept_df['staff_id'], 'first_name': staff_dept_df['first_name'], 'last_name': staff_dept_df['last_name'], 'department_name': staff_dept_df['department_name'], 'location': staff_dept_df['location'], 'email_address': staff_dept_df['email_address']})
+    dim_staff.to_csv('./tmp/dim_staff.csv', index=False)
 
 def dim_design():
     des_df = pd.read_csv('./tmp/design.csv')
@@ -25,3 +28,5 @@ def dim_design():
 def dim_location():
     # location_id, address_line_1, address_line_2, district, city, postal_code, country, phone
     pass
+
+dim_staff()
