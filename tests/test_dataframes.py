410c53507635bef39803c0f38aab052b433539c0
Chon Lee <chonlee@Chons-Mac-mini.local>
2023-02-24 15:04:25 +0000

    dim tables created

--- tests/test_dataframes.py
+++ tests/test_dataframes.py
@@ -1,4 +1,4 @@
-from src.dataframes import get_file_names, read_csv_files, tmp_exists
+from src.dataframes import create_dim_staff
 import csv
 import pytest
 import os
@@ -6,15 +6,35 @@ import os
 if not os.path.exists('./tmp'):
     os.makedirs('./tmp')
 
-def test_get_file_names_returns_a_list_of_all_file_names_in_tmp_directory():
-    file_names = ['purchase_order.csv', 'sales_order.csv', 'counterparty.csv', 'staff.csv', 'currency.csv', 'department.csv', 'address.csv', 'payment.csv', 'payment_type.csv', 'transaction.csv', 'design.csv']
-    for file_name in file_names:
-        assert file_name in get_file_names()
+def test_dim_staff_star_schema_contains_correct_column():
+    correct_column_names = ['staff_id', 'first_name',
+                            'last_name', 'department_name', 'location', 'email_address']
+    dim_staff_table = create_dim_staff()
+    staff_names = [key for key in dim_staff_table]
+    assert staff_names == correct_column_names
 
-def test_dataframe_and_csv_file_contains_same_length_of_columns():
-    with open('./tmp/staff.csv', 'r', encoding = 'utf-8') as csv_file:
+
+def test_staff_department_id_correlates_with_department_name():
+    department_look_up = {
+        'Sales':	1,
+        'Purchasing':	2,
+        'Production':	3,
+        'Dispatch':	4,
+        'Finance':	5,
+        'Facilities':	6,
+        'Communications':	7,
+        'HR':	8
+    }
+
+    with open('./tmp/staff.csv', 'r', encoding='utf-8') as csv_file:
         csv_data = csv.reader(csv_file)
         csv_list = [data for data in csv_data]
-        pd_data = read_csv_files()
-        pd_list = [df for df in pd_data[9]]
-        assert len(csv_list[0]) == len(pd_list)
+
+    with open('./tmp/dim_staff.csv', 'r', encoding='utf-8') as dim_staff_file:
+        dim_staff_data = csv.reader(dim_staff_file)
+        dim_staff_list = [data for data in dim_staff_data]
+
+    for index in range(1, len(dim_staff_list)):
+        compare_value = department_look_up[dim_staff_list[index][3]]
+        assert compare_value == int(csv_list[index][3])
+    
